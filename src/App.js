88fc0ed586382909dc20e4b2fdff07dbc4d30584
Lady Dzidzor <ladydzidzor@gmail.com>
2020-10-06 09:46:48 +0000

    created a component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import FirstComponent from './FirstComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    <FirstComponent />
+      <FirstComponent />
+    <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FirstComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function FirstComponent() {
+    return (
+        <p>This is the first component</p> 
+            )
+}
+
+export default FirstComponent;
